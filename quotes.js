482129cca97ac37f696fe871cc986ea3081e3055
ela_lisondra <emrlisondra@gmail.com>
2021-07-03 12:41:00 +0100

    fetch superhi quotes api, finish css design with bgc transition, add colour transition using js, add quotation marks and dash in ajax

--- quotes.js
+++ quotes.js
@@ -1,28 +1,43 @@
 const quoteTag = document.querySelector("h1")
 const authorTag = document.querySelector("p")
+const bodyTag = document.querySelector("body")
 const randomTag = document.querySelector("footer img")
 
 let data = []
 
-fetch("quotes.json")
-    .then( response => response.json())
-    .then( jsonData => {
-        data = jsonData
-        getQuote()
-})
+// fetch("https://api.superhi.com/api/test/quotes/random")
+//     .then( response => response.json())
+//     .then( jsonData => {
+//         data = jsonData
+//         getQuote()
+// })
 
 const getQuote = function () {
 
-    if (data.length > 0) {
+//     if (data.length > 0) {
         
-        const randomNumber = Math.floor(Math.random() * data.length)
-        const randomQuote = data[randomNumber]
+//         const randomNumber = Math.floor(Math.random() * data.length)
+//         const randomQuote = data[randomNumber]
 
-        quoteTag.innerHTML = randomQuote.quote
-        authorTag.innerHTML = randomQuote.author
-    }
+//         quoteTag.innerHTML = randomQuote.quote
+//         authorTag.innerHTML = randomQuote.author
+//     }
     
+    
+    fetch("https://api.superhi.com/api/test/quotes/random")
+    .then(response => response.json())
+    .then(jsonData => {
+        quoteTag.innerHTML = "&ldquo;" + jsonData.quote + "&rdquo;"
+        authorTag.innerHTML = "&mdash;" + jsonData.author
 
+        if (jsonData.quote.length > 100) {
+            quoteTag.classList.add("long")
+        } else {
+            quoteTag.classList.remove("long")
+        }
+
+        bodyTag.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 90%)`
+    })
 }
 
 getQuote()
